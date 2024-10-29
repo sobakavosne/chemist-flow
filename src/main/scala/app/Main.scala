09bdf23bd963f42d298a5961793ebf003a85b0a4
mr. dima <sobakavosne@gmail.com>
2024-10-29 01:39:23 +0100

    feat(all): add `cats-effect` & add `circe`

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -2,21 +2,57 @@ package app
 
 import akka.actor.ActorSystem
 import api.Endpoints
-import akka.stream.Materializer
+import cats.effect.{ExitCode, IO, IOApp, Resource}
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
 
-object Main extends App {
-  implicit val system: ActorSystem        = ActorSystem("ChemistActorSystem")
-  implicit val materializer: Materializer = Materializer(system)
+object Main extends IOApp {
+  def actorSystemResource(
+    implicit
+    ec: ExecutionContext,
+    system: ActorSystem
+  ): Resource[IO, ActorSystem] =
+    Resource.make(IO(system))(system =>
+      IO(
+        system
+          .terminate()
+          .onComplete {
+            case Success(_) =>
+              println("Actor system terminated successfully")
+            case Failure(ex) =>
+              println(s"Actor system termination failed: ${ex.getMessage}")
+          }
+      ).handleErrorWith(ex => IO(println(s"Failed to terminate actor system: ${ex.getMessage}")))
+    )
 
-  val host = sys.env.getOrElse("CHEMIST_FLOW_HOST", "0.0.0.0")
-  val port = sys.env.getOrElse("CHEMIST_FLOW_PORT", "8081").toInt
+  def endpointsResource: Resource[IO, Endpoints] =
+    Resource.make(IO(new Endpoints))(endpoints =>
+      IO(println("Shutting down Endpoints")).handleErrorWith(_ => IO.unit)
+    )
 
-  val endpoints = new Endpoints()
-  endpoints.startServer(host, port)
+  def runApp(
+    host: String,
+    port: Int
+  )(
+    implicit
+    ec: ExecutionContext,
+    system: ActorSystem
+  ): Resource[IO, Unit] =
+    for {
+      system    <- actorSystemResource
+      endpoints <- endpointsResource
+      _         <- endpoints.startServer(host, port)(system)
+      _         <- Resource.eval(IO(scala.io.StdIn.readLine))
+    } yield ()
 
-  scala.io.StdIn.readLine()
+  override def run(
+    args: List[String]
+  ): IO[ExitCode] = {
+    implicit val system: ActorSystem  = ActorSystem("ChemistFlowActorSystem")
+    implicit val ec: ExecutionContext = system.dispatcher
+    val host: String                  = sys.env.getOrElse("CHEMIST_FLOW_HOST", "0.0.0.0")
+    val port: Int                     = sys.env.getOrElse("CHEMIST_FLOW_PORT", "8081").toInt
 
-  sys.addShutdownHook {
-    system.terminate()
+    runApp(host, port).use(_ => IO.unit).as(ExitCode.Success)
   }
 }
--- src/main/scala/utility/Common.scala
+++ src/main/scala/utility/Common.scala
@@ -2,6 +2,8 @@ package utility
 
 object FunctionOps {
   implicit class FunctionComposition[A, B](f: A => B) {
-    def ~(g: A => A): A => B = f.compose(g)
+    require(f != null, "Function must be non-null")
+
+    def ◦[C](g: C => A): C => B = f compose g
   }
 }
