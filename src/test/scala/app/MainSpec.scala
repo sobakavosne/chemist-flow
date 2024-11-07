d0ba709a000cb85f9b0c4a42748c66320cf65865
mr. dima <sobakavosne@gmail.com>
2024-11-08 00:30:13 +0100

    fix(test): remove unused implicit

--- src/test/scala/app/MainSpec.scala
+++ src/test/scala/app/MainSpec.scala
@@ -9,19 +9,16 @@ import com.comcast.ip4s.Port
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AsyncWordSpec
-import org.typelevel.log4cats.Logger
-import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 class MainSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll {
 
   "Main" should {
     "start the http4s server as a Resource" in {
-      implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
-      implicit val endpoints          = new Endpoints
-      val serverBuilder               = new ServerBuilder
-      val maybeHost                   = Host.fromString("0.0.0.0")
-      val maybePort                   = Port.fromInt(8081)
-      val bindingResource             = serverBuilder.startServer(maybeHost.get, maybePort.get)
+      implicit val endpoints = new Endpoints
+      val serverBuilder      = new ServerBuilder
+      val maybeHost          = Host.fromString("0.0.0.0")
+      val maybePort          = Port.fromInt(8081)
+      val bindingResource    = serverBuilder.startServer(maybeHost.get, maybePort.get)
 
       bindingResource
         .use { server =>
