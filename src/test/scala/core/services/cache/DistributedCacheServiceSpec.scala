a666ad09cef86a1f28e3762fff07079d00d34640
mr. dima <sobakavosne@gmail.com>
2024-11-21 01:43:59 +0100

    fix(test): add missing `DistributedCacheServiceTest` cases

--- src/test/scala/core/services/cache/DistributedCacheServiceSpec.scala
+++ src/test/scala/core/services/cache/DistributedCacheServiceSpec.scala
@@ -18,8 +18,12 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 import scala.concurrent.duration._
+import core.domain.preprocessor.MechanismDetails
+import core.domain.preprocessor.FOLLOW
+import core.domain.preprocessor.Stage
+import core.domain.preprocessor.ReactionDetails
 
-class DistributedCacheServiceTest extends AnyFunSuite with Matchers with BeforeAndAfterAll {
+class DistributedCacheServiceSpec extends AnyFunSuite with Matchers with BeforeAndAfterAll {
 
   val config: Config                            = DefaultConfigLoader.pureConfig
   val system: ActorSystem                       = ActorSystem("ChemistFlowActorSystem", config)
@@ -68,6 +72,29 @@ class DistributedCacheServiceTest extends AnyFunSuite with Matchers with BeforeA
     retrievedMechanism shouldEqual mechanism
   }
 
+  test("putMechanismDetails and getMechanismDetails should store and retrieve a mechanism details") {
+    val mechanismId: MechanismId           = 1
+    val mechanism: Mechanism               = Mechanism(mechanismId, "mechanism", "type", 1.0)
+    val mechanismDetails: MechanismDetails = MechanismDetails(
+      mechanismContext  = (mechanism, FOLLOW("description")),
+      stageInteractants = List((Stage(10, "name", "description", List()), List()))
+    )
+
+    val result = for {
+      _         <- cacheService.putMechanismDetails(mechanismId, mechanismDetails)
+      retrieved <- retryIO(10, 500.millis) {
+                     cacheService.getMechanismDetails(mechanismId).flatMap {
+                       case Some(value) => IO.pure(value)
+                       case None        => IO.raiseError(new Exception("Mechanism not yet replicated"))
+                     }
+                   }
+    } yield retrieved
+
+    val retrievedMechanismDetails = result.unsafeRunSync()
+
+    retrievedMechanismDetails shouldEqual mechanismDetails
+  }
+
   test("putReaction and getReaction should store and retrieve a reaction") {
     val reactionId: ReactionId = 1
     val reaction: Reaction     = Reaction(reactionId, "reaction")
@@ -85,6 +112,26 @@ class DistributedCacheServiceTest extends AnyFunSuite with Matchers with BeforeA
     val retrievedReaction = result.unsafeRunSync()
 
     retrievedReaction shouldEqual reaction
+  }
+
+  test("putReactionDetails and getReactionDetails should store and retrieve a reaction") {
+    val reactionId: ReactionId           = 1
+    val reaction: Reaction               = Reaction(reactionId, "reaction")
+    val reactionDetails: ReactionDetails = ReactionDetails(reaction, List(), List(), List())
+
+    val result = for {
+      _         <- cacheService.putReactionDetails(reactionId, reactionDetails)
+      retrieved <- retryIO(10, 500.millis) {
+                     cacheService.getReactionDetails(reactionId).flatMap {
+                       case Some(value) => IO.pure(value)
+                       case None        => IO.raiseError(new Exception("Reaction not yet replicated"))
+                     }
+                   }
+    } yield retrieved
+
+    val retrievedReactionDetails = result.unsafeRunSync()
+
+    retrievedReactionDetails shouldEqual reactionDetails
   }
 
   private def retryIO[A](retries: Int, delay: FiniteDuration)(action: IO[A]): IO[A] = {
